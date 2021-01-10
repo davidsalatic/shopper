eb3c4b77e40d834e52699970f8186cfc8955e078
David Salatic <davidsalatic@live.com>
2021-01-11 00:13:47 +0100

    registration logic moved to AuthService

--- src/main/java/com/shopper/auth/service/AuthService.java
+++ src/main/java/com/shopper/auth/service/AuthService.java
@@ -1,4 +1,8 @@
 package com.shopper.auth.service;
 
+import com.shopper.auth.dto.RegistrationDto;
+import com.shopper.shopper.dto.ShopperDto;
+
 public interface AuthService {
+    ShopperDto register(RegistrationDto registrationDto);
 }
